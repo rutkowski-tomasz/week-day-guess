1a74d9023328e8f290de0a76877e83d64981ce05
rutkowski-tomasz <rutkowski-tomasz@users.noreply.github.com>
2022-03-22 18:19:11 +0000

    deploy: 489d19f630e8af497e2541c99f9b0f70cf8a1ab2

---
+++ precache-manifest.21b60e025496fb02b7522d4815406787.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "68c0895dcc2e15c25f7d",
+    "url": "/week-day-guess/static/css/main.5be55302.chunk.css"
+  },
+  {
+    "revision": "68c0895dcc2e15c25f7d",
+    "url": "/week-day-guess/static/js/main.68c0895d.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/week-day-guess/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "e5d44de3bb30651085cd",
+    "url": "/week-day-guess/static/css/2.c214d338.chunk.css"
+  },
+  {
+    "revision": "e5d44de3bb30651085cd",
+    "url": "/week-day-guess/static/js/2.e5d44de3.chunk.js"
+  },
+  {
+    "revision": "c2acff46101450007d26",
+    "url": "/week-day-guess/static/js/runtime~main.c2acff46.js"
+  },
+  {
+    "revision": "b29a888ff7f07091c7e08eb0d991e221",
+    "url": "/week-day-guess/static/media/primeicons.b29a888f.ttf"
+  },
+  {
+    "revision": "e01fd4133bac49cd2ea07ad6f7c45695",
+    "url": "/week-day-guess/static/media/primeicons.e01fd413.eot"
+  },
+  {
+    "revision": "943c3597cd33be56d53df0d1982fa8ff",
+    "url": "/week-day-guess/static/media/primeicons.943c3597.woff"
+  },
+  {
+    "revision": "64b5d470af63a67aa9b9d3e8a866a35e",
+    "url": "/week-day-guess/static/media/primeicons.64b5d470.svg"
+  },
+  {
+    "revision": "0f9d09995123c31ecd25dfac6d33707f",
+    "url": "/week-day-guess/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/week-day-guess/precache-manifest.ff8057ec2ab9250b2df9c4e58607465f.js"
+  "/week-day-guess/precache-manifest.21b60e025496fb02b7522d4815406787.js"
 );
 
 workbox.clientsClaim();
