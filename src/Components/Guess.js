e9b7d04c6de9657138be4c331749b71ebeec20e3
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2022-03-23 09:15:03 +0100

    Improve responsive design

--- src/Components/Guess.js
+++ src/Components/Guess.js
@@ -2,29 +2,20 @@ import React from 'react';
 
 import {Button} from "primereact/button";
 import useDateHelpers from "../Helpers/DateHelpers";
-import {Dropdown} from "primereact/dropdown";
 
 function Guess({ hasGuessed, onGenerateNewGuess, onGuess }) {
 
     const {formatWeekDay} = useDateHelpers();
 
-    const dropdownItems = [...Array(7)].map((_, i) => ({ label: formatWeekDay(i), value: i }));
-
     return hasGuessed
         ? <div className="buttons">
-            <Button label="New guess" onClick={() => { console.log('tse'); return onGenerateNewGuess() }} />
+            <Button label="New guess" onClick={() => onGenerateNewGuess()} />
         </div>
-        : <>
-            <div className="buttons full">
-                {[...Array(7)].map((_, i) =>
-                    <Button key={i} label={formatWeekDay(i)} onClick={() => onGuess(i)} />
-                )}
-            </div>
-            <div className="buttons mobile">
-                <Dropdown options={dropdownItems} onChange={(e) => onGuess(e.value)} placeholder="Select day of the week"/>
-            </div>
-        </>
-
+        : <div className="buttons">
+            {[...Array(7)].map((_, i) =>
+                <Button key={i} label={formatWeekDay(i)} onClick={() => onGuess(i)} />
+            )}
+        </div>
     ;
 }
 
